81e14af41610c5b69aa9f4492784c2f30db1021a
Mikhail <skakunovmy@gmail.com>
2021-04-16 17:18:45 +0300

    HomeWork2

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -2,6 +2,35 @@ package Lesson2;
 
 public class Lesson2 {
     public static void main(String[] args) {
-        System.out.println();
+
+        bool10Ore20(5, 17);
+        printConsole(1);
+        positiveNegativeNumber(0);
+        printLine("JAVA",3);
+
+    }
+    public static boolean bool10Ore20(int a, int d) {
+        int sum = a + d;
+    if (sum >= 10 && sum <= 20) {
+        return true;
+    }
+        else {return false;}
+    }
+    public static void printConsole(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное.");
+        }
+        else {
+            System.out.println("Число отрицательное.");
+        }
+    }
+    public static boolean positiveNegativeNumber(int a) {
+        if (a >= 0) { return false; }
+        else { return true; }
+    }
+    public static void printLine(String str, int a) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(str);
+        }
     }
 }
