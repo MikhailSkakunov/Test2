04dd5c9316b6d37f21ddb321a63e96def4348f4f
Mikhail <skakunovmy@gmail.com>
2021-04-26 08:48:36 +0300

    HomeWork4

--- src/Lesson3/lesson3.java
+++ src/Lesson3/lesson3.java
@@ -10,6 +10,8 @@ public class lesson3 {
         square();
         mas(6, 3);
         minMaxValue();
+        equalSum (new int[] {2, 2, 2, 2, 2, 2, 3, 3, 3, 1, 1, 1});
+        moveValue();
     }
 
         public static void mas() {
@@ -38,9 +40,11 @@ public class lesson3 {
                   if (array[i] < 6) {
                       array[i] *=2;
                   }
-                  System.out.print(array[i] + " ");
+                  System.out.print(Arrays.toString(array) + " ");
               }
+            System.out.println();
         }
+
 
     public static void square() {
         int[][] sq = new int[5][5];
@@ -64,7 +68,6 @@ public class lesson3 {
             }
             System.out.println();
         }
-  //      System.out.println();
     }
     public static int[] mas(int len, int initialValue) {
 
@@ -79,17 +82,49 @@ public class lesson3 {
     public static void minMaxValue() {
         int[] ar = {6, 3, 12, 18, 0, -1, 2, -3};
         for (int i = 0; i < ar.length; i++) {
-            int min = 0;
-            int max = 0;
+            int min = ar[0];
+            int max = ar[0];
             if (ar[i] < min) {
                 min = ar[i];
                 System.out.println(min);
             }
-
             if (ar[i] > max) {
                 max = ar[i];
                 System.out.println(max);
             }
         }
     }
-}
+    public static boolean equalSum( int[] arr) {
+     int sum1 = 0;
+     int sum2 = 0;
+     for (int i = 0; i < arr.length; i++) {
+         sum1 += arr[i];}
+         System.out.println(sum1);
+
+     for (int j = arr.length - 1; j >= 0; j--) {
+         sum2 += arr[j];
+         int sum3 = sum1 - sum2;
+         System.out.println(sum2 + " " + sum3);
+         if (sum3 != sum2) {
+             continue;
+         } else if (sum3 == sum2) {
+             System.out.println(sum2 + " " + sum3);
+             System.out.println(sum2);
+             break;
+         }
+     }
+     System.out.println("true");
+     return true;
+ }
+
+    public static void moveValue() {
+     int[] val = {1, 2, 3, 4, 5};
+     int n = 1;
+     for (int i = 0; i < val.length - n; i++) {
+         int j = val[i];
+         val[i] = val[i + n];
+         val[i + n] = j;}
+         for (int a : val)
+             System.out.print(a + " ");}
+        }
+
